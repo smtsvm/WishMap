05221e6badbf4dcf04c9d869add625986e73afff
Samet Sevim <Samet.Sevim@logo.com.tr>
2016-10-21 14:43:21 +0300

    Fix primary color

--- app/src/main/java/wishmap/com/tr/wishmap/LoginActivity.java
+++ app/src/main/java/wishmap/com/tr/wishmap/LoginActivity.java
@@ -1,20 +1,30 @@
 package wishmap.com.tr.wishmap;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
+        loginButton = (Button) findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
